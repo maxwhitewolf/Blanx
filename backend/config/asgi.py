0b358aabbc5564f57f73087c28cca048e44ae2e0
maxwhitewolf <nsaijayanth221@gmail.com>
2025-07-27 14:48:25 +0530

    Add ProtocolTypeRouter to ASGI

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,10 +1,17 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import get_default_application
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Standard Django ASGI application
 django_asgi_app = get_asgi_application()
 
 import config.routing
-application = config.routing.application 
+
+# Route HTTP requests to Django and WebSocket connections to the
+# existing Channels URL router.
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": config.routing.application,
+})
