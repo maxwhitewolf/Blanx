c90f2d5b9598fa92355ea076c1a5b34f5a994584
maxwhitewolf <nsaijayanth221@gmail.com>
2025-07-27 14:15:50 +0530

    Use env vars for DB credentials

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -62,8 +62,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB', 'socialmedia'),
-        'USER': 'postgres',            # or your custom user if you created one
-        'PASSWORD': 'Jayanth123',   # the password you set
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
