431240896aee6cf5330ec0911a5e21bd3d7bac37
maxwhitewolf <nsaijayanth221@gmail.com>
2025-07-27 17:37:12 +0530

    Fix placeholder screens

---
+++ mobile/src/screens/ProfileScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ProfileScreen = () => (
+  <View style={styles.container}>
+    <Text>Profile Screen - TODO</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' }
+});
+
+export default ProfileScreen;
